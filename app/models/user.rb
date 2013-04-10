d43fc63d2abc37072d5d53867d214aa7757b93f7
blakeshall <blake@blakeshall.com>
2013-04-10 10:22:47 -0400

    Remove user stubs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :trackable, :validatable,
+  devise :trackable,
          :omniauthable, :omniauth_providers => [:cheddar]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :cheddar_id, :cheddar_username, :cheddar_url, :cheddar_token
   # attr_accessible :title, :body
-
-  def email
-    "Stub"
-  end
-
-  def email_changed?
-    false
-  end
-
-  def password
-    "stubstub"
-  end
 
   def self.find_for_cheddar_oauth(auth, signed_in_resource=nil)
     user = User.where(:cheddar_id => auth.extra.raw_info.id).first
