3ed924991c6a218d1cf1712be2039ab7c23c7ed1
blakeshall <blake@blakeshall.com>
2013-04-10 09:24:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheeseBoard::Application.config.secret_token = '1d7edf4a0c6d5c620e362f796121eb4867ca6e19a2dff5e1f9f157a482b79409e3fdf2a4f83ed575bb21132c0f1f487dbde5f81ea906d6d2c791033e76a33ced'
