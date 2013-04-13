5d34d34663b5e928b7477b030946fb4f39d4ab0e
blakeshall <blake@blakeshall.com>
2013-04-12 21:44:19 -0400

    Asset precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module CheeseBoard
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
+    config.assets.precompile << '*.css'
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
